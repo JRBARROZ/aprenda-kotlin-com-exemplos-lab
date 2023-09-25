50f839d034e6aec3cc444115b8c395300f01cb2a
Jhonatas Rodrigues de Barros <40250320+JRBARROZ@users.noreply.github.com>
2023-09-25 20:39:00 -0300

    finishing challenge

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,59 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val matricula: Number, val name: String){
+    init {
+        println("Novo Candidato: $name - $matricula")
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val courseLevel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
+    private val inscritos = mutableListOf<Usuario>()
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    fun alunosMatriculados() : MutableList<Usuario>{
+        return inscritos
+    }
+
+    fun removerMatricula(matricula: Number){
+        for(inscrito in inscritos){
+            if(inscrito.matricula == matricula){
+                inscritos.remove(inscrito)
+                return
+            }
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val conteudos: List<ConteudoEducacional> = listOf<ConteudoEducacional>(
+        ConteudoEducacional("Javascript", 30, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("CSS", 25, Nivel.BASICO),
+        ConteudoEducacional("HTML", 10, Nivel.BASICO),
+    )
+
+    val students: List<Usuario> = listOf<Usuario>(
+        Usuario(1, "Jhonatas"),
+        Usuario(2, "Fábio"),
+        Usuario(3, "Arthur")
+    )
+
+    val formacaoFrontEnd = Formacao("Front-End", conteudos)
+
+    for (student in students){
+        formacaoFrontEnd.matricular(student)
+    }
+
+    println(formacaoFrontEnd.alunosMatriculados())
+
+    println(formacaoFrontEnd.removerMatricula(1))
+
+    println(formacaoFrontEnd.alunosMatriculados())
+
 }
